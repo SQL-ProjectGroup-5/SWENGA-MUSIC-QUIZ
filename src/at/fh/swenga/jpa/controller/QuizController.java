0a522ba6bfe319cdb3e1c4cbc72c0f8fbb2cfe61
GZEI <29224009+GZEI@users.noreply.github.com>
2019-06-17 21:55:50 +0200

    Added actual play functionality

--- src/at/fh/swenga/jpa/controller/QuizController.java
+++ src/at/fh/swenga/jpa/controller/QuizController.java
@@ -1,8 +1,12 @@
 package at.fh.swenga.jpa.controller;
 
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,51 +37,80 @@ public class QuizController {
 	SongRepository songRepository;
 	@Autowired
 	DocumentRepository documentRepository;
+
 	@RequestMapping(value = { "/quizzes", "listquizzes" })
 	public String index(Model model) {
 		List<QuizModel> quizzes = quizRepository.findAll();
 		model.addAttribute("quizzes", quizzes);
 		return "indexQuiz";
 	}
-	
+
 	@RequestMapping("/")
 	public String showIndex(Model model) {
 		return "login";
 	}
+
 	@RequestMapping("/fillquizzes")
 	@Transactional
 	public String fillData(Model model) {
 		DataFactory df = new DataFactory();
 		Calendar publishDate = Calendar.getInstance();
 		publishDate.setTime(df.getDateBetween(df.getDate(2000, 1, 1), df.getDate(2019, 1, 1)));
-		QuizModel quizModel = new QuizModel ("Test",1,publishDate);
+		QuizModel quizModel = new QuizModel("Test", 1, publishDate);
 		quizModel.setSongs(songRepository.findAll());
 		quizRepository.save(quizModel);
 		return "forward:listquizzes";
 	}
 
-	@RequestMapping(value="/savequiz",method = RequestMethod.POST)
+	@RequestMapping(value = "/savequiz", method = RequestMethod.POST)
 	@Transactional
 	public String saveData(@RequestParam String quizname, Model model) {
 		DataFactory df = new DataFactory();
 		Calendar publishDate = Calendar.getInstance();
 		publishDate.setTime(df.getDateBetween(df.getDate(2000, 1, 1), df.getDate(2019, 1, 1)));
-		QuizModel quizModel = new QuizModel (quizname,1,publishDate);
+		QuizModel quizModel = new QuizModel(quizname, 1, publishDate);
 		quizModel.setSongs(songRepository.findAll());
 		quizRepository.save(quizModel);
 		return "forward:quizManagement";
 	}
+
 	@RequestMapping(value = "/play")
-	public String handlePlay(@RequestParam(value = "gid") int gid, @RequestParam(value = "nickname") String nickname, @RequestParam(value = "qid", required = false) int qid, HttpSession session, Model model) {
+	public String handlePlay(@RequestParam(value = "gid") int gid, @RequestParam(value = "nickname") String nickname,
+			@RequestParam(value = "qid", required = false) int qid, HttpSession session, Model model) {
 		model.addAttribute("gameIndex", gid);
-		model.addAttribute("questionIndex", qid+1);
-		model.addAttribute("nickname", nickname);
-		return "game";
+		Optional<QuizModel> quizOpt = quizRepository.findById(gid);
+		if (!quizOpt.isPresent()) {
+			model.addAttribute("errorMessage","Wrong ID");
+			return "login";
+		} else {
+			QuizModel quiz = quizOpt.get();
+			List<SongModel> currSongs = new ArrayList<SongModel>(quiz.getSongs());
+			if (qid < currSongs.size()) {
+				SongModel currQuestion = currSongs.get(qid);
+				model.addAttribute("currDocument", currQuestion.getDocument().getId());
+				model.addAttribute("questionIndex", qid + 1);
+				model.addAttribute("nickname", nickname);
+				List<String> possibleAnswers = new ArrayList<String>();
+				possibleAnswers.add(currQuestion.getAnswer1());
+				possibleAnswers.add(currQuestion.getAnswer2());
+				possibleAnswers.add(currQuestion.getAnswer3());
+				possibleAnswers.add(currQuestion.getTitle());
+				Collections.shuffle(possibleAnswers);
+				model.addAttribute("possibleAnswers", possibleAnswers);
+				
+			} else {
+				model.addAttribute("message","You finished!");
+				return "login";
+			}
+			return "game";
+		}
 	}
+
 	@RequestMapping("/quizStatistics")
 	public String showQStatistics(Model model) {
 		return "quizmaster";
 	}
+
 	@RequestMapping("/quizAdmin")
 	@Transactional
 	public String showQuizAdmin(Model model) {
@@ -85,6 +118,7 @@ public class QuizController {
 		model.addAttribute("songs", songs);
 		return "quiz";
 	}
+
 	@RequestMapping("/quizManagement")
 	@Transactional
 	public String showQuizzes(Model model) {
@@ -93,13 +127,14 @@ public class QuizController {
 		return "quizManagement";
 	}
 
-	
 	@RequestMapping(value = "qrcode/{id}", method = RequestMethod.GET)
-	public void qrcode(@PathVariable("id") String id, HttpServletResponse response,HttpServletRequest request) throws Exception {
+	public void qrcode(@PathVariable("id") String id, HttpServletResponse response, HttpServletRequest request)
+			throws Exception {
 		response.setContentType("image/png");
 		OutputStream outputStream = response.getOutputStream();
-		String reqSrc = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/quizzes/";
-		outputStream.write(ZXingHelper.getQRCodeImage(reqSrc+id, 200, 200));
+		String reqSrc = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+				+ "/quizzes/";
+		outputStream.write(ZXingHelper.getQRCodeImage(reqSrc + id, 200, 200));
 		outputStream.flush();
 		outputStream.close();
 	}
